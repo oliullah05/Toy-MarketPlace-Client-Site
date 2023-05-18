60ea194f7ad579ccd39cd7904f8b2c1c3762899a
oliullah05 <mdolihasan@gmail.com>
2023-05-18 21:30:32 +0600

    review section done

--- src/Pages/Home/Review/Review.jsx
+++ src/Pages/Home/Review/Review.jsx
@@ -8,8 +8,8 @@ const Review = () => {
 <div className="grid mb-8 border container mx-auto border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
     <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
         <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">ToyZone Delivers Excellence: A Customer's Perspective</h3>
-            <p className="my-5">ToyZone surpassed my expectations with their impeccable service and exceptional toy car collection. From the user-friendly website to the prompt delivery, every aspect of my purchase was seamless. The quality and attention to detail in their toy cars left me in awe. ToyZone is my go-to destination for an unforgettable toy car experience.</p>
+            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Matico Delivers Excellence: A Customer's Perspective</h3>
+            <p className="my-5">Matico surpassed my expectations with their impeccable service and exceptional toy car collection. From the user-friendly website to the prompt delivery, every aspect of my purchase was seamless. The quality and attention to detail in their toy cars left me in awe. Matico is my go-to destination for an unforgettable toy car experience.</p>
         </blockquote>
         <figcaption className="flex items-center justify-center space-x-3">
             <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture"/>
@@ -24,8 +24,8 @@ const Review = () => {
     
     <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-tr-lg dark:bg-gray-800 dark:border-gray-700">
         <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Unforgettable Toy Car Adventure: A Customer's Journey with ToyZone</h3>
-            <p className="my-4">My adventure with ToyZone was nothing short of amazing. Their extensive selection of toy cars catered to my every desire, and the smooth purchasing process made it a breeze to bring them into my collection. The quality and craftsmanship of their toy cars surpassed my expectations, transporting me to a world of joy and nostalgia.</p>
+            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Unforgettable Toy Car Adventure: A Customer's Journey with Matico</h3>
+            <p className="my-4">My adventure with Matico was nothing short of amazing. Their extensive selection of toy cars catered to my every desire, and the smooth purchasing process made it a breeze to bring them into my collection. The quality and craftsmanship of their toy cars surpassed my expectations, transporting me to a world of joy and nostalgia.</p>
         </blockquote>
         <figcaption className="flex items-center justify-center space-x-3">
             <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture"/>
@@ -37,7 +37,7 @@ const Review = () => {
     </figure>
     <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-bl-lg md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
         <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Unmatched Toy Shopping Experience at ToyJoy</h3>
+            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Unmatched Toy Shopping Experience at Matico</h3>
             <p className="my-4">"I am thrilled with my recent purchase from FunToyWorld. Their website offered a vast selection of toys, making it easy to find the perfect one. The toy I received was of exceptional quality, and the attention to detail was impressive. The customer service team was friendly and responsive</p>
         </blockquote>
         <figcaption className="flex items-center justify-center space-x-3">
@@ -50,8 +50,8 @@ const Review = () => {
     </figure>
     <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-br-lg dark:bg-gray-800 dark:border-gray-700">
         <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Unmatched Toy Shopping Experience at ToyJoy</h3>
-            <p className="my-4">ToyJoy exceeded my expectations in every way. Their website was user-friendly, making it effortless to browse and find the ideal toy for my child. The toy I bought arrived promptly and was packaged securely. The quality and durability of the toy were outstanding, ensuring it will withstand hours of playtime. </p>
+            <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Unmatched Toy Shopping Experience at Matico</h3>
+            <p className="my-4">Matico exceeded my expectations in every way. Their website was user-friendly, making it effortless to browse and find the ideal toy for my child. The toy I bought arrived promptly and was packaged securely. The quality and durability of the toy were outstanding, ensuring it will withstand hours of playtime. </p>
         </blockquote>
         <figcaption className="flex items-center justify-center space-x-3">
             <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture"/>
